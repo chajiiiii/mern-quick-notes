c960d160378d3f6058916590727d83962c8684fd
chajiiiii <j.cha1708@gmail.com>
2023-08-18 09:23:49 +0100

    Part 1 & 2

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+
+// Always require and configurate near the top
+require("dotenv").config();
+
+require("./config/database");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+
+// Configure both serve-favicon & static middleware
+// to serve from the production 'build' folder
+app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
+app.use(express.static(path.join(__dirname, "build")));
+
+// Put API routes here, before the "catch all" route
+
+// sei.com/order/1
+app.get("/order/:id", function (req, res) {
+  // return JSON
+});
+
+// sei.com/dashboard
+// The following "catch all" route (note the *) is necessary
+// to return the index.html on all non-AJAX requests
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, function () {
+  console.log(`Express app running on port ${port}`);
+});
---
+++ src/pages/App/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./App.css";
+import AuthPage from "../AuthPage/AuthPage";
+import NewOrderPage from "../NewOrderPage/NewOrderPage";
+import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
+import NavBar from "../../components/NavBar/NavBar";
+
+import { Routes, Route } from "react-router-dom";
+
+export default function App() {
+  const [user, setUser] = useState({});
+
+  return (
+    <main>
+      {user ? (
+        <>
+          <NavBar />
+          <Routes>
+            <Route path="/orders" element={<OrderHistoryPage />} />
+            <Route path="/orders/new" element={<NewOrderPage />} />
+          </Routes>
+        </>
+      ) : (
+        <AuthPage />
+      )}
+    </main>
+  );
+}
