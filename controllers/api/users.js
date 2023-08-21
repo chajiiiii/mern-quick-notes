c25b7440905a86e6127cab637c585a299c52244c
chajiiiii <j.cha1708@gmail.com>
2023-08-21 15:16:49 +0100

    Part 5

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,12 +1,31 @@
+const jwt = require("jsonwebtoken");
+const User = require("../../models/user");
+
 module.exports = {
   create,
 };
 
-function create(req, res) {
-  res.json({
-    user: {
-      name: req.body.name,
-      email: req.body.email,
-    },
-  });
+async function create(req, res) {
+  try {
+    // Add the user to the database
+    const user = await User.create(req.body);
+
+    // token will be a string
+    const token = createJWT(user);
+    res.json(token);
+  } catch (err) {
+    // 400 = bad request
+    res.status(400).json(err);
+  }
+}
+
+// Helper Functions
+
+function createJWT(user) {
+  return jwt.sign(
+    // data payload
+    { user },
+    process.env.SECRET,
+    { expiresIn: "24h" }
+  );
 }
